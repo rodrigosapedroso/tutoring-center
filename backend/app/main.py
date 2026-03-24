bf649258868cb3ece74a58a0d8210035c84e2080
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-03-24 13:16:02 +0000

    run uvicorn server

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+#File to create the app FastAPI, import routes and connect them to the app
+from fastapi import FastAPI
+
+app = FastAPI(title="Tutoring Center")
+
+@app.get("/") 
+async def root(): 
+    return {
+        "status": "ok",
+        "service": "Tutoring Center API"
+    }
