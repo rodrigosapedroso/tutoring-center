6d8f617514ff1d1fd23cc6d878398d9578b8737b
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-04-01 23:56:02 +0100

    define the total discipline list available in the tutoring center

---
+++ backend/app/seeds/discipline_list.py
@@ -0,0 +1,43 @@
+from ..models import DisciplineLevel
+
+DISCIPLINES = {
+        DisciplineLevel.BASIC: [
+            "Português",
+            "História e Geografia de Portugal", 
+            "Matemática",
+            "Inglês",
+            "Fisico-Química",
+            "Ciências Naturais",
+            "Francês",
+            "Espanhol",
+            "Alemão",
+        ],
+        DisciplineLevel.SECONDARY: [
+            "Biologia e Geologia",
+            "Matemática A",
+            "Matemática B",
+            "MACS",
+            "Geometria Descritiva",
+            "Economia",
+            "Filosofia",
+            "Psicologia",
+            "Português",
+            "Físico-Química",
+            "História A",
+            "Física A",
+        ],
+        DisciplineLevel.HIGHER: [
+            "Álgebra",
+            "Anatomia",
+            "Apoio a teses",
+            "Contabilidade",
+            "Direito",
+            "Avaliação de Projetos",
+            "Python",
+            "Gestão",
+            "Economia",
+            "Química Orgânica",
+            "Química Geral",
+            "Física"
+        ],
+    }
