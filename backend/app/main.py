dd7342e21480579b65b722386d106dbc34b9900a
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-04-01 23:49:24 +0100

    include auth, teachers and discipline routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,12 @@
 #File to create the app FastAPI, import routes and connect them to the app
-
 from dotenv import load_dotenv
+
 # Load .env variables into environment
 load_dotenv()
 
 from fastapi import FastAPI
 from .database import Base, engine
-from .routers import auth_router
+from .routers import auth_router, discipline_router, teachers_router
 from . import models
 
 app = FastAPI(title="Tutoring Center")
@@ -23,3 +23,5 @@ Base.metadata.create_all(bind=engine)
 
 # Include routers
 app.include_router(auth_router.router)
+app.include_router(discipline_router.router)
+app.include_router(teachers_router.router)
