8009a65cba01f6cd5cc2e8881dc1cadd1b65da5e
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-04-02 12:26:50 +0100

    define a service to create student

---
+++ backend/app/services/student_service.py
@@ -0,0 +1,35 @@
+import uuid
+
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from ..schemas import StudentCreate
+from ..models import Student, Parent
+
+
+def create_student(student_data: StudentCreate, db: Session):
+    # Create student profile
+    student = Student(
+        id=str(uuid.uuid4()),
+        name=student_data.name,
+        birth=student_data.birth,
+        nationality=student_data.nationality,
+        contact=student_data.contact,
+    )
+
+    # Associate parents (if given)
+    if student.parent_ids:
+        parents = db.query(Parent).filter(
+            Parent.id.in_(student.parent_ids)
+        ).all()
+
+        if len(parents) != len(student.parent_ids):
+            raise HTTPException(status_code=404, detail="One or more parents not found")
+
+        student.parents = parents
+        
+    db.add(student)
+    db.commit()
+    db.refresh(student)
+    
+    return student
