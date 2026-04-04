acf58d9069aaf4e10c835e1c604e0490a77b4be3
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-04-04 20:36:06 +0100

    include get_students endpoint to return student list for admin and teacher management

--- backend/app/routers/students_router.py
+++ backend/app/routers/students_router.py
@@ -1,22 +1,32 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from ..services.student_service import create_student
+from ..services.student_service import create_student as create_student_service, get_students as get_students_service
 from ..database import get_db
 from ..models import User
-from ..schemas import StudentCreate, StudentRead
-from ..auth import require_admin
+from ..schemas import StudentCreate, StudentList, StudentRead
+from ..auth import require_admin, get_current_user
 
 router = APIRouter(prefix="/api/students")
 
 @router.post("/", response_model=StudentRead)
-def create_student_endpoint(
+def create_student(
     student_data: StudentCreate,
     db: Session = Depends(get_db),
     _: User = Depends(require_admin)
 ):
     try:
-        student = create_student(student_data, db)
+        student = create_student_service(student_data, db)
         return student
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.get("/", response_model=list[StudentList])
+def get_students(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    students = get_students_service(current_user.id, current_user.role, db)
+    return students
+
