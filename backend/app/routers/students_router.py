4dce1f948ca687e6b4a710306960c152634d240c
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-04-05 11:21:57 +0100

    add two routers: patch to partially update student, and delete

--- backend/app/routers/students_router.py
+++ backend/app/routers/students_router.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from ..services.student_service import create_student as create_student_service, get_students as get_students_service, get_student_by_id
+from ..services.student_service import (
+    create_student as create_student_service,
+    get_students as get_students_service,
+    get_student_by_id,
+    update_student as update_student_service,
+    delete_student as delete_student_service,
+)
 from ..database import get_db
 from ..models import User
-from ..schemas import StudentCreate, StudentList, StudentRead
+from ..schemas import StudentCreate, StudentList, StudentRead, StudentUpdate
 from ..auth import require_admin, get_current_user
 
 router = APIRouter(prefix="/api/students")
@@ -35,3 +41,24 @@ def get_student(
     current_user: User = Depends(get_current_user)
 ):
     return get_student_by_id(student_id, current_user, db)
+
+
+@router.patch("/{student_id}", response_model=StudentRead)
+def update_student(
+    student_id: str,
+    student_data: StudentUpdate,
+    db: Session = Depends(get_db),
+    _: User = Depends(require_admin)
+):
+    student = update_student_service(student_id, student_data, db)
+    return student
+
+
+@router.delete("/{student_id}")
+def delete_student(
+    student_id: str,
+    db: Session = Depends(get_db),
+    _: User = Depends(require_admin)
+):
+    return delete_student_service(student_id, db)
+
